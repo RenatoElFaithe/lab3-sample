660211294a24617931e645329c103b5dbfc33f55
Desarrollador <Desarrollador@LAB02-PC12>
2025-03-31 16:23:20 -0500

    add student model

--- src/main/java/com/tecsup/lab03/lab03_sample/model/Student.java
+++ src/main/java/com/tecsup/lab03/lab03_sample/model/Student.java
@@ -1,12 +1,16 @@
 package com.tecsup.lab03.lab03_sample.model;
 
 public class Student {
-	private String name;
-	private int ege;
-	
-	
-	public Student (Strign name, int ege) {
-		this.name = name;
-		this.ege = ege;
-	}
+    
+    private String name;
+    private int age;
+    
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+    
+    public String getName() {return name;}
+    public int getAge() {return age;}
 }
+
